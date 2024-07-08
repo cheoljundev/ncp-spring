36b46e458d219cbf518392bcd0761603ae0ef710
cheoljundev <cheoljundev@gmail.com>
2024-07-08 17:06:45 +0900

    add file

---
+++ src/main/java/hello/ncpspring/NcpspringApplication.java
@@ -0,0 +1,13 @@
+package hello.ncpspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcpspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcpspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/ncpspring/NcpspringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.ncpspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcpspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
